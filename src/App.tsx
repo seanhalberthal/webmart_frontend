8d6591e4a6f618395eadf85862f7891a7f515c2d
bssmnt <socuting@gmail.com>
2025-05-08 15:56:39 +0100

    updated react-router version because of a vulnerability

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import './App.css';
 import Products from "./pages/products.tsx";
 import ProductDetail from "./pages/ProductDetail";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import "@radix-ui/themes/styles.css";
 
 function App() {
-  return (
-    <Router>
-      <div className="flex flex-col items-center min-h-screen text-center bg-auto">
-        <div className="pt-10">
-          <h1 className="text-3xl font-bold">Welcome to WebMart!</h1>
-        </div>
-        <div className="mt-16 w-full">
-          <Routes>
-            <Route path="/" element={<Products />} />
-            <Route path="/products/:id" element={<ProductDetail />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+                <div className="flex flex-col items-center min-h-screen text-center bg-auto">
+                    <div className="pt-10">
+                        <h1 className="text-3xl font-bold">Welcome to WebMart!</h1>
+                    </div>
+                    <div className="mt-16 w-full">
+                        <Routes>
+                            <Route path="/" element={<Products/>}/>
+                            <Route path="/products/:id" element={<ProductDetail/>}/>
+                        </Routes>
+                    </div>
+                </div>
+        </Router>
+    );
 }
 
 export default App;
